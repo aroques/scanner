8a0184569b6be209000994272d4e463f15a52f9b
Alec Roques <alec.roques@gmail.com>
2018-09-29 14:48:03 -0500

    Remove comment from scanner

--- src/test_scanner.cpp
+++ src/test_scanner.cpp
@@ -8,13 +8,10 @@ void test_scanner()
 {
     Token token = get_token();
     
-    int cnt = 0;
-    while (cnt < 3)//(token.type != END_OF_FILE_TK)
+    while (token.type != END_OF_FILE_TK)
     {
         std::cout << token.instance << std::endl;
         token = get_token();
-
-        cnt++;
     }
     
 }
